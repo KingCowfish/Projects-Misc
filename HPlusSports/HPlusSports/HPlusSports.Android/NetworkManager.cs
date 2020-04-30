698e2ff17a42f4e21dfe6d320d76c1c37566dfe5
KingCowfish <brodera146@gmail.com>
2020-04-30 13:07:06 -0700

    Finish class

---
+++ HPlusSports/HPlusSports/HPlusSports.Android/NetworkManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net;
+
+[assembly:Xamarin.Forms.Dependency(typeof(HPlusSports.Droid.NetworkManager))]
+namespace HPlusSports.Droid 
+{
+    public class NetworkManager : INetworkManager
+    {
+        Context ctx = Android.App.Application.Context;
+        public NetworkManager()
+        {
+        }
+
+        public bool IsNetworkConnected()
+        {
+            var cSvc = (ConnectivityManager)ctx.GetSystemService(Context.ConnectivityService);
+
+            return cSvc.ActiveNetworkInfo != null &&
+                cSvc.ActiveNetworkInfo.IsConnected;
+        }
+    }
+}
